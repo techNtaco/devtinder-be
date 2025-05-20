2b123e941278a64be57a948208404d6246efe784
techNtaco <techntaco@gmail.com>
2025-05-20 19:24:54 +0530

    Chat feature fix.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,5 @@ import os from 'os';
 
 export const isProd = os.hostname().startsWith('ip-');
 export const FRONTEND_ORIGIN = isProd
-  ? 'http://65.0.95.115'   // replace with your real domain/IP
+  ? 'http://65.0.95.115'
   : 'http://localhost:5173';
